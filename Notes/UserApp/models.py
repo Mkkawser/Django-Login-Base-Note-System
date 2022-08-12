0c41b10948efbd7d4684a5fa401a8ee35034925f
Kawser Hossain <109532781+Mkkawser@users.noreply.github.com>
2022-08-12 17:38:28 +0600

    Delete Notes directory

--- Notes/UserApp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-from ckeditor.fields import RichTextField
-
-
-class note_model(models.Model):
-    note_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    note_title = models.CharField(max_length=255)
-    note_desc = RichTextField()
--- Notes/postNote/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from ckeditor.fields import RichTextField
-
-
-class post_model(models.Model):
-    post_user = models.CharField(max_length=255)
-    post_title = models.CharField(max_length=255)
-    post_desc = RichTextField()
